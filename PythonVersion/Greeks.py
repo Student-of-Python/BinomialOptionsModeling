69c2c969761f3554cff401cb612a85a9c08c03e2
Gleb S <gsokhin2024@gmail.com>
2025-12-23 13:25:00 -0500

    Added Gamma to the Greeks

--- PythonVersion/Greeks.py
+++ PythonVersion/Greeks.py
@@ -21,13 +21,14 @@ class Greeks:
 
     def apply_delta(self, parent: Node, right: Optional[Node] , left: Optional[Node]) -> None:
         """
-        :param parent:
-        :param right:
-        :param left:
-        :return: In-place calculation
+        :param parent: parent Node or leaf node
+        :param right: Right node (If applicable)
+        :param left: Left node (If applicable)
+        :return: None; In-place calculation
 
+        Notes:
         Delta = Change in Option Value  / Change in Stock Price
-        --> V_U - V_D / S_U - S_D
+            --> V_U - V_D / S_U - S_D
 
         For Terminal Nodes:
         Delta = 1 --> ITM
@@ -47,27 +48,48 @@ class Greeks:
 
     def apply_gamma(self, parent: Node, right: Optional[Node] , left: Optional[Node]):
         """
-        :param parent:
-        :param right:
-        :param left:
-        :return:
+        :param parent: parent Node or leaf node
+        :param right: Right node (If applicable)
+        :param left: Left node (If applicable)
+        :return: None; In-place calculation
+
+        Notes:
+        Gamma = Change in Delta / Change in Stock Price
+            --> D_U - D_D / S_U - S_D
+        For Terminal Nodes:
+        Gamma = 0 --> ITM
+        Gamma = 0 --> OTM
+        Gamma = inf. --> ATM
+        (ITM, OTM delta is solidified, with inf at ATM since it switches from 0-1 in instantly)
         """
-        pass
+
+
+        if not right or not left:
+            # leaf Node
+
+            #TODO: Gamma behaves weirdly around expiration; should I omit Gamma from last two levels?
+            gamma = 0 if (parent.delta != self.strike) else -1
+            parent.gamma = gamma
+            return
+        else:
+
+            parent.gamma = (right.delta - left.delta) / (right.stock_value - left.stock_value)
+            return
 
 
     def compute_greeks(self) -> None:
         """
         :return:
-
         """
 
         for i in range(len(self.i_tree) - 1, -1, -1): #O(N) time with 2^height - 1 elems :(
             parent = self.i_tree[i]
             right = self.i_tree[2 * i + 2] if 2 * i + 2 < len(self.i_tree) - 1 else None
             left = self.i_tree[2*i + 1] if 2*i + 1 < len(self.i_tree) - 1 else None
+
+            #Apply functions
             self.apply_delta(parent,right, left)
-
-
+            self.apply_gamma(parent, right, left)
 
 
 
@@ -78,7 +100,7 @@ class Greeks:
         """
         tree_str = ""
         for i in range(self.height + 1):
-            tree_str += " ".join([f"{node.delta:.2f}" for node in self.i_tree[2**i-1: 2**(i+1) - 1]]) + "\n"
+            tree_str += " ".join([f"{node.gamma:.2f}" for node in self.i_tree[2**i-1: 2**(i+1) - 1]]) + "\n"
         return tree_str
 
 
